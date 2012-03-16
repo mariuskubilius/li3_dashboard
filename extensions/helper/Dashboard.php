58eef8211a83a878620d1b586baf84261fb0d83a
Marius Kubilius <marius.kubilius@gmail.com>
2012-03-16 02:00:08 +0200

    Initial commit

---
+++ extensions/helper/Dashboard.php
@@ -0,0 +1,11 @@
+<?php
+namespace li3_dashboard\extensions\helper;
+
+class Dashboard extends \lithium\template\Helper {
+	
+	protected $_classes = array(
+		'breadcrumbs' => '\li3_dashboard\AdminNav',
+	);
+	
+}
+?>
